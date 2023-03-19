c7a4569d7057dd160c9e613bc47fc95ce414d707
Anshuman Bhardwaj <anshu5074@gmail.com>
2023-03-19 16:07:29 +0530

    fix rendering issue

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -1,13 +1,14 @@
 import React from "react";
+import Footer from "@theme-original/Footer";
 import Docsly from "@docsly/base";
 import "@docsly/base/index.css";
 import { useLocation } from "@docusaurus/router";
 
-export default function Root({ children }) {
+export default function FooterWrapper(props) {
   const { pathname } = useLocation();
   return (
     <>
-      {children}
+      <Footer {...props} />
       <Docsly
         publicId="public_bsKNacuWYmCdRNEEt5qyI8-XdYxoURcugzU3"
         pathname={pathname}
